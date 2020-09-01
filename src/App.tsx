e9fd53db952e380f924ca5d99dbe5bec12d800dc
fuuki <fuuki.shigenaka@kuno-corp.com>
2020-09-01 15:23:47 +0900

    feat: サンプルファイルを修正

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => {
     <div className="App">
       <Provider store={store}>
         <div className="no_print">
-          <a href="https://github.com/fuuki/label-generator">
+          <a href="https://github.com/fuuki/label-generator/blob/master/instructions/README_ja.md#%E6%A8%99%E6%9C%AC%E3%83%A9%E3%83%99%E3%83%AB%E4%BD%9C%E6%88%90%E3%83%84%E3%83%BC%E3%83%AB">
             使い方はこちら（How to use）
         </a>
           <Setting />
